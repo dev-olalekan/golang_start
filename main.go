b3662de62ed0141faf52be4065cdeb23314798ef
olalekan <olalekanotuyiga@gmail.com>
2025-10-31 12:48:42 +0100

    project scaffolding

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+/* only one main function in the entire application */
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+/* to run the file in terminal use  go run main.go */
